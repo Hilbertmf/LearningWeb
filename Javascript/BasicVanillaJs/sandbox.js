60fe8ce1803442633a37e1c7a0c77fd204de67e4
hilbert <19953198+Hilbertmf@users.noreply.github.com>
2019-08-17 13:34:40 -0300

    .

--- Javascript/BasicVanillaJs/sandbox.js
+++ Javascript/BasicVanillaJs/sandbox.js
@@ -1,20 +1,20 @@
 // sample function
 function solve(string) {
-    string = string.split("");
+    string = string.split('');
     let answer = 0;
 
-    for(let index of string) {
-        if(index === "0" || index === "6" || index === "9")
+    for(const index of string) {
+        if(index === '0' || index === '6' || index === '9')
             answer += 6;
-        else if(index === "1") 
+        else if(index === '1') 
             answer += 2;
-        else if(index === "2" || index === "3" || index === "5") 
+        else if(index === '2' || index === '3' || index === '5') 
             answer += 5;
-        else if(index === "4")
+        else if(index === '4')
             answer += 4;
-        else if(index === "7") 
+        else if(index === '7') 
             answer += 3;
-        else if(index === "8") 
+        else if(index === '8') 
             answer += 7;
     }
     return answer;
@@ -22,13 +22,13 @@ function solve(string) {
 
 
 // gets the input
-let form = document.getElementById('myForm');
+let form = document.getElementsByClassName('myForm')[0];
 
 form.addEventListener('submit', function(e) {
     let textArea = document.getElementById('field');
     let input = textArea.value;
     let answer = solve(input);
-    document.getElementById("result").innerHTML += answer + " leds<br>  ";
+    document.getElementById('result').innerHTML += `The answer is ${answer}!<br>`;
     e.preventDefault();
 });
 

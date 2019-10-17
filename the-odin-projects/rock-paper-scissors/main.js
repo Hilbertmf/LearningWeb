adac90fd02e4d7c8b3f38ae42a813e1d8d66877c
hilbert <19953198+Hilbertmf@users.noreply.github.com>
2019-10-17 18:45:29 -0300

    new function: gameController

--- the-odin-projects/rock-paper-scissors/main.js
+++ the-odin-projects/rock-paper-scissors/main.js
@@ -1,3 +1,10 @@
+// Variables
+let playerChoice, computerChoice, playerScore, computerScore, rounds;
+const playerChoices = document.querySelectorAll('.player-choices a');
+const paper  = playerChoices[0];
+const rock = playerChoices[1];
+const scissors = playerChoices[2];
+
 const getRandomInteger = function(min, max) {
      return Math.floor(Math.random() * (max - min + 1) + min);
 };
@@ -16,92 +23,130 @@ const getComputerPlay = function() {
     return play;
 };
 
-const getRoundResult = function(playerChoice) {
-    
-    playerChoice = playerChoice.toLowerCase();
-    computerChoice = getComputerPlay();
-    let result;
-    if(playerChoice === computerChoice)
-        result = 'draw!';
-    else if(playerChoice === 'rock') 
-        result = computerChoice === 'paper' ? 'computer wins!' : 'human wins!';
-    else if(playerChoice === 'paper') 
-        result = computerChoice === 'scissors' ? 'computer wins!' : 'human wins!';
-    else if(playerChoice === 'scissors')
-        result = computerChoice === 'rock' ? 'computer wins!' : 'human wins!';
+const getRoundResult = function(playerChoice, computerChoice) {
+    let result = new Array(3);
+    if(playerChoice === computerChoice) {
+        result[0] = 'It\'s a tie!';
+        result[1] = 'draw';
+    }
+    else if(playerChoice === 'rock') {
+        if(computerChoice === 'paper') {
+            result[0] = 'You Lose! Paper beats Rock!';
+            // winner
+            result[1] = 'computer';
+        }
+        else {
+            result[0] = 'You Win! Rock beats Scissors!';
+            // winner
+            result[1] = 'human';
+        }
+    }
+    else if(playerChoice === 'paper') {
+        if(computerChoice === 'scissors') {
+            result[0] = 'You Lose! Scissors beats Paper';
+            // winner
+            result[1] = 'computer';
+        }
+        else {
+            result[0] = 'You win! Paper beats Rock!';
+            // winner
+            result[1] = 'human';
+        }
+    }
+    else if(playerChoice === 'scissors') {
+        if(computerChoice === 'rock') {
+            result[0] = 'You Lose! Rock beats Scissors';
+            // winner
+            result[1] = 'computer';
+        }
+        else {
+            result[0] = 'You win! Scissors beats Paper!';
+            // winner
+            result[1] = 'human';
+        }
+    }
     
     return result;
 };
 
-const gameRound = function() {
-
-    // Get the choices:
-    const playerChoices = document.querySelectorAll('.player-choices a');
-    const paper = playerChoices[0];
-    const rock = playerChoices[1];
-    const scissor = playerChoices[2];
-    console.log('choices = ', playerChoices);
-    console.log('paper = ', paper);
-    console.log('rock = ', rock);
-    console.log('scissor = ', scissor);
-    
-    
-    let playerChoice = window.prompt('Write rock, paper or scissors');
-    let computerChoice = getComputerPlay();
+const gameRound = function(playerChoice) {
+    computerChoice = getComputerPlay();
     
     console.log('Computer plays: ', computerChoice, ' | Human plays: ', playerChoice);
     let result = getRoundResult(playerChoice, computerChoice);
-    console.log(result);
+    console.log(result[0]);
 
-    return result.split(' ')[0]; // winner
+    return result[1]; // winner
 };
 
-const game = function () {
+
+const gameController = function(playerChoice) {
+    if(rounds === 0) {
+        console.log('Computer Score: ', computerScore);
+        console.log('Human Score: ', playerScore);
+    }
+
+    let winner = gameRound(playerChoice);
     
-    let computerScore = 0;
-    let playerScore = 0;
+    if(winner == 'computer') {
+        computerScore++;
+    }
+    else if(winner == 'human') {
+        playerScore++;
+    }
+
     console.log('Computer Score: ', computerScore);
     console.log('Human Score: ', playerScore);
 
-    for (let i = 0; i < 5; i++) {
-        
-        let winner = gameRound();
-        if(winner === 'computer') {
-            computerScore++;
-        }
-        else if(winner === 'human') {
-            playerScore++;
-        }
-        
-        console.log('Computer Score: ', computerScore);
-        console.log('Human Score: ', playerScore);
-    } 
-    
-    if(computerScore === playerScore)
+    if(rounds === 4) {
+        if(computerScore === playerScore) {
         console.log('It\'s a draw!');
-    else if(computerScore > playerScore) 
-        console.log('Computer wins! I\'m sorry Dave. I\'m afraid I can\'t do that.');
-    else
-        console.log('Human wins!');
-
+        }
+        else if(computerScore > playerScore) {
+            console.log('Computer wins! I\'m sorry Dave. I\'m afraid I can\'t do that.');
+        }
+        else {
+            console.log('Human wins!');
+        }
+    }
+    rounds++;
 };
-
 // game();
 
-//get the player choices
-const playerChoiceLinks = document.querySelectorAll('.player-choices a');
-const paper = playerChoiceLinks[0];
-const rock = playerChoiceLinks[1];
-const scissor = playerChoiceLinks[2];
+// get buttons
+const newGame = document.querySelector('.reset #newgame-btn');
 
-let playerChoice, computerChoice;
+// When click New Game
+newGame.addEventListener('click', () => {
+    // Set counters to 0
+    // Initialize variables
+    playerScore = 0;
+    computerScore = 0;
+    playerChoice = '';
+    computerChoice = '';
+    rounds = 0;
+    
+    // change classes from disabled to enabled
+    playerChoices.forEach(choice => {
+        choice.classList.remove('disabled');
+        choice.classList.add('enabled');
+    });
+    // enable finish game
+    // disable newgame itself
 
-paper.addEventListener('click', () => {
-    playerChoice = 'paper';
-    alert(getRoundResult(playerChoice));
 });
 
-rock.addEventListener('click', () => alert('click me again'));
+paper.addEventListener('click', () => {
+    if(paper.classList.contains('enabled')) {
+        playerChoice = 'paper';
+        
+        gameController(playerChoice);
 
-scissor.addEventListener('click', () => alert('click me again'));
+        if(rounds === 5) {
+            // finishGame();
+        }
+
+        // Check if it's the last round
+    }
+});
 
